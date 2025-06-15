c8e849cb050c5e3309ca6d12c2d08b6b7de624e3
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-06-15 18:06:35 +0530

    prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
 
 function App() {
-  
-// console.log("hey") // it gives eslint error
-  return (
-    <>
-     <div>
-       Hello.
-     </div>
-    </>
-  )
+    // console.log("hey") // it gives eslint error
+    return (
+        <>
+            <div>Hello</div>
+        </>
+    )
 }
 
 export default App
