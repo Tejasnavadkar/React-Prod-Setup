758ef44b9058d2408cdde774458258a0d609592f
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-06-15 19:15:21 +0530

    stylelint setup

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import eslintConfigPrettier from 'eslint-config-prettier'  // to avoid conflicts 
+import eslintConfigPrettier from 'eslint-config-prettier'  // to avoid conflicts
 
 export default tseslint.config(
     { ignores: ['dist', 'node_modules', 'build'] },
