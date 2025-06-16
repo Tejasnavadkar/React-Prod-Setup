f3649a2746d1bab98a66f8fe142956126ffa3a77
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-06-16 22:14:39 +0530

    env validation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,56 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()]
+// export default defineConfig({
+//     plugins: [react(), tailwindcss()]
+// })
+
+type TMode = 'development' | 'production'
+
+interface envVariables {
+    PORT: string
+    VITE_ENV: string
+}
+
+function envValidation(allEnvVariables: envVariables, envMode: TMode) {
+    const env: (keyof envVariables)[] = ['PORT', 'VITE_ENV']
+    env.forEach((element) => {
+        if (!Object.keys(allEnvVariables).includes(element)) {
+            throw new Error(`${element} is missing from ${envMode}`)
+        }
+    })
+}
+
+function normalisePort(port: string): number {
+    const value = parseInt(port)
+    if (isNaN(value)) throw new Error('invalid PORT Number')
+    return value
+}
+
+export default defineConfig(({ mode }) => {
+    // here we destructure mode form obj
+
+    const envMode = mode as TMode
+    // console.log('mode--',mode)
+    const allEnvVariables = loadEnv(envMode, process.cwd(), '') as unknown as envVariables // loadEnv is fn that provides vite that load all env's from current-working-directory(cwd)
+
+    envValidation(allEnvVariables, envMode)
+
+    // now create fn to convert port string into number
+    const port = normalisePort(allEnvVariables.PORT)
+
+    const config = {
+        port,
+        open: true
+    }
+
+    return {
+        plugins: [react(), tailwindcss()],
+        server: config, // now when we run our react app it runs on this port not a default 5173 port
+        preview: config,
+        build: {
+            minify: true // make build code in minify version
+        }
+    }
 })
